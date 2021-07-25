7f0f77e95695f688fd9a50e79fd5b482086fc8ca
Samwell <biosamuel7@gmail.com>
2021-07-25 03:05:03 +0000

    Revieew code

--- src/app/pages/mail/mail.page.ts
+++ src/app/pages/mail/mail.page.ts
@@ -19,25 +19,28 @@ export class MailPage implements OnInit {
     this.http.get<any[]>('https://devdactic.fra1.digitaloceanspaces.com/gmail/data.json')
     .subscribe(res => {
       console.log(res);
-      this.emails = res;
+      this.emails = res.map(email => {
+        email.color = this.intToRGB(this.hashCode(email.from));
+        return email;
+      });
     });
 
   }
 
   // https://stackoverflow.com/questions/3426404/create-a-hexadecimal-colour-based-on-a-string-with-javascript
-  // private hashCode(str) {
-  //   let hash = 0;
-  //   for (let i = 0; i < str.length; i++) {
-  //     hash = str.charCodeAt(i) + ((hash << 5) - hash);
-  //   }
-  //   return hash;
-  // }
+  private hashCode(str) {
+    let hash = 0;
+    for (let i = 0; i < str.length; i++) {
+      hash = str.charCodeAt(i) + ((hash << 5) - hash);
+    }
+    return hash;
+  }
 
-  // private intToRGB(i) {
-  //   let c = (i & 0x00FFFFFF)
-  //     .toString(16)
-  //     .toUpperCase();
+  private intToRGB(i) {
+    let c = (i & 0x00FFFFFF)
+      .toString(16)
+      .toUpperCase();
 
-  //   return '#' + '00000'.substring(0, 6 - c.length) + c;
-  // }
+    return '#' + '00000'.substring(0, 6 - c.length) + c;
+  }
 }
